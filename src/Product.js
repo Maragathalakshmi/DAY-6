3f3b0ff6ede123d8fe904104aad74306226916da
Maragathalakshmi <108513716+Maragathalakshmi@users.noreply.github.com>
2023-04-06 22:50:00 +0530

    Product.js

---
+++ src/Product.js
@@ -0,0 +1,19 @@
+import React from 'react'
+function Productlist(props) {
+    const { products } = props;
+    return (
+        <div><br /><br /><br />
+            {products.map((product) => (
+                <div>
+                    {product.no}.
+                        Name : {product.name}&nbsp;&nbsp;
+                    Price : {product.price}&nbsp;&nbsp;
+                    ImageURL : {product.img}&nbsp;&nbsp;
+                    
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default Productlist;
